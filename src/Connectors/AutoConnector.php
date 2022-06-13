68beeaf032fe9361aeed0cefad65680a64905529
Magomedcoder <info@magomedcoder.ru>
2022-06-13 18:12:13 +0300

    Added base, pool, auto Connector

---
+++ src/Connectors/AutoConnector.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PCore\Redis\Connectors;
+
+use ArrayObject;
+use PCore\Redis\Contracts\ConnectorInterface;
+use PCore\Redis\RedisConfig;
+use Swoole\Coroutine;
+
+/**
+ * Class AutoConnector
+ * @package PCore\Redis\Connectors
+ * @github https://github.com/pcore-framework/redis
+ */
+class AutoConnector implements ConnectorInterface
+{
+
+    protected array $connectors = [
+        'pool' => PoolConnector::class,
+        'base' => BaseConnector::class
+    ];
+    protected ?ArrayObject $pool = null;
+
+    public function __construct(protected RedisConfig $config)
+    {
+        $this->pool = new ArrayObject();
+    }
+
+    public function get(): \Redis
+    {
+        $type = $this->getConnectorType();
+        if (!$this->pool->offsetExists($type)) {
+            $connector = $this->connectors[$type];
+            $this->pool->offsetSet($type, new $connector($this->config));
+        }
+
+        return $this->pool->offsetGet($type)->get();
+    }
+
+    public function release($redis)
+    {
+        $this->pool->offsetGet($this->getConnectorType())->release($redis);
+    }
+
+    protected function getConnectorType(): string
+    {
+        return class_exists(Coroutine::class) && Coroutine::getCid() > 0 ? 'pool' : 'base';
+    }
+
+}
---
+++ src/Connectors/BaseConnector.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PCore\Redis\Connectors;
+
+use PCore\Redis\Contracts\ConnectorInterface;
+use PCore\Redis\Redis;
+use PCore\Redis\RedisConfig;
+
+/**
+ * Class BaseConnector
+ * @package PCore\Redis\Connectors
+ * @github https://github.com/pcore-framework/redis
+ */
+class BaseConnector implements ConnectorInterface
+{
+
+    protected \ArrayObject $pool;
+
+    public function __construct(protected RedisConfig $config)
+    {
+        $this->pool = new \ArrayObject();
+    }
+
+    public function get(): \Redis
+    {
+        $name = $this->config->getName();
+        if (!$this->pool->offsetExists($name)) {
+            $redis = new \Redis();
+            $redis->connect(
+                $this->config->getHost(),
+                $this->config->getPort(),
+                $this->config->getTimeout(),
+                $this->config->getReserved(),
+                $this->config->getRetryInterval(),
+                $this->config->getReadTimeout()
+            );
+            $redis->select($this->config->getDatabase());
+            if ($auth = $this->config->getAuth()) {
+                $redis->auth($auth);
+            }
+            $this->pool->offsetSet($name, $redis);
+        }
+
+        $redis = $this->pool->offsetGet($name);
+        $this->pool->offsetUnset($name);
+        return $redis;
+    }
+
+    public function release($redis)
+    {
+        $this->pool->offsetSet($this->config->getName(), $redis);
+    }
+
+}
